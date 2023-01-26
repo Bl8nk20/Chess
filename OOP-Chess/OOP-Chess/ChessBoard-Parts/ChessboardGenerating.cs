b613a203d154876ae12c8ad78ebb6b4c37cc1d0a
ms85866 <ms85866@LAP420-0607>
2023-01-26 13:59:27 +0100

    codestructure update

---
+++ OOP-Chess/OOP-Chess/ChessBoard-Parts/ChessboardGenerating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class GeneratingChessBoard
+{
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public GeneratingChessBoard()
+	{
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	~GeneratingChessBoard() { }
+}
